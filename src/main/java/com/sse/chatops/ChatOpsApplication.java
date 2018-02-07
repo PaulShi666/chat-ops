afc7f5d50fdc6e520e444fa3562e4ff38d5868f7
ctshi <ctshi@tc.com>
2018-02-07 11:31:01 +0800

    Initial commit

---
+++ src/main/java/com/sse/chatops/ChatOpsApplication.java
@@ -0,0 +1,11 @@
+package com.sse.chatops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatOpsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ChatOpsApplication.class, args);
+	}
+}
